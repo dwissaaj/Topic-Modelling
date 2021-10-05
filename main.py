0e2f0108b5ad98ca6f7c773b5b9ed747a8350fcb
dwissaaj <dwis.buntelman123@gmail.com>
2021-10-05 12:20:15 +0700

    Done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tes_json
 import nltk
 import pyLDAvis
+
 import pyLDAvis.gensim_models
 import spacy
 w_tokenizer = nltk.tokenize.WhitespaceTokenizer()
@@ -55,11 +56,30 @@ data['cols_to_check'] = data['teks'].replace({
 data['check'] = data['cols_to_check'].str.replace(r'[https]+[?://]+[^\s<>"]+|www\.[^\s<>"]+[@?()]+[(??)]+[)*]+[(\xa0]+[-&gt...]', "",regex=True)
 
 
-'''def lemmatize_text(text):
+
+def lemmatize_text(text):
     return [lemmatizer.lemmatize(w) for w in w_tokenizer.tokenize(text)]
 
-data['lemma'] = data.check.apply(lemmatize_text)'''
+data['lemma'] = data.check.apply(lemmatize_text)
+
+lemma = data['lemma']
 
 
+id2word = corpora.Dictionary(lemma)
 
+corpus = []
+for text in lemma:
+    new = id2word.doc2bow(text)
+    corpus.append(new)
 
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                            id2word=id2word,
+                                            num_topics=20,
+                                            random_state=100,
+                                            update_every=1,
+                                            chunksize=100,
+                                            passes=10,
+                                            alpha='auto')
+py = pyLDAvis.gensim_models.prepare(lda_model,corpus,id2word,mds='mmds',R=30)
+
+x = pyLDAvis.save_html(py,'viz.html')
